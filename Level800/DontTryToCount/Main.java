fe1096c6684889e55a9f6d1faef934114f2a65a4
Dhananjai Ghawalkar <dhananjaighawalkar68@gmail.com>
2026-08-27 23:33:25 +0530

    Add Don't Try to Count solution

---
+++ Level800/DontTryToCount/Main.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+
+            String x = sc.next();
+            String s = sc.next();
+
+            System.out.println(solve(n, m, x, s));
+        }
+    }
+
+    public static int solve(int n, int m, String x, String s) {
+
+        // your logic here
+
+        return 0;
+    }
+}
