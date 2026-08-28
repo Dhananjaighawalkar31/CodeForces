63b208d667c9ebd72cae1820135134c2c5902210
Dhananjai Ghawalkar <dhananjaighawalkar68@gmail.com>
2026-08-28 18:50:47 +0530

    Add How Much Does Daytona Cost solution

---
+++ Level800/HowMuchDoesDaytonaCost/Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            System.out.println(solve(n, k, arr));
+        }
+    }
+
+    public static String solve(int n, int k, int[] arr) {
+
+        // your logic
+
+        return "";
+    }
+}
