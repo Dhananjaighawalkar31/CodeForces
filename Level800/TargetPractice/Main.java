a7087b0ca7932b60ba44f7e03e7eddc0e19f91ea
Dhananjai Ghawalkar <dhananjaighawalkar68@gmail.com>
2026-08-28 23:05:43 +0530

    Add Target Practice solution

---
+++ Level800/TargetPractice/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+
+            // read 10 x 10 grid
+
+            System.out.println(solve(/* grid */));
+        }
+    }
+
+    public static int solve(/* grid */) {
+
+        // your logic
+
+        return 0;
+    }
+}
