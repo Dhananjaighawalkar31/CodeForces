df8c52843975f44519a66ea0b1f32bdf9cf5253b
Dhananjai Ghawalkar <dhananjaighawalkar68@gmail.com>
2026-08-28 23:19:25 +0530

    Add Target Practice solution

--- Level800/TargetPractice/Main.java
+++ Level800/TargetPractice/Main.java
@@ -13,37 +13,65 @@ public class Main {
             char[][] grid = new char[10][10];
 
             for (int i = 0; i < 10; i++) {
+
                 String row = sc.next();
 
                 for (int j = 0; j < 10; j++) {
+
                     grid[i][j] = row.charAt(j);
+
                 }
+
             }
 
             System.out.println(solve(grid));
+
         }
+
     }
 
     public static int solve(char[][] grid) {
 
-        int score = 0;
+        int c = 0;
 
-        for (int i = 0; i < 10; i++) {
+        for(int i = 0; i < 10; i++) {
 
-            for (int j = 0; j < 10; j++) {
+            for(int j = 0; j < 10; j++) {
 
-                if (grid[i][j] == 'X') {
+                char ch = grid[i][j];
 
-                    int ring = Math.min(
-                        Math.min(i, 9 - i),
-                        Math.min(j, 9 - j)
-                    ) + 1;
+                if(ch == 'X') {
 
-                    score += ring;
+                    if(i == 0 || j == 0 || j == 9 || i == 9) {
+
+                        c++;
+
+                    }else if(i == 1 || j == 1 || j == 8 || i == 8) {
+
+                        c += 2;
+
+                    }else if(i == 2 || j == 2 || j == 7 || i == 7) {
+
+                        c += 3;
+
+                    }else if(i == 3 || j == 3 || i == 6 || j == 6) {
+
+                        c += 4;
+
+                    }else if(i == 4 || j == 4 || i == 5 || j == 5) {
+
+                        c += 5;
+
+                    }
+
                 }
+
             }
+
         }
 
-        return score;
+        return c;
+
     }
+
 }
